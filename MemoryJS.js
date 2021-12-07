4f062f75c7f48e04342b003d82d25bcf798d1a76
Yael Chen <51058584+YaelChen@users.noreply.github.com>
2021-12-07 11:55:23 +0200

    fixed classes + notes!

--- MemoryJS.js
+++ MemoryJS.js
@@ -1,25 +1,26 @@
+//a function to shuffle the array with each load to make a new game:
 function shuffle(array) {
-    var m = array.length,
+    let m = array.length,
         last, rand;
 
-    // While there remain elements to shuffle - while True…
+    //While there remain elements to shuffle - while True…
     while (m) {
 
-        // Pick a remaining element randomly
+        //Pick a remaining element randomly
         rand = Math.floor(Math.random() * m--);
 
-        // Swap it with the current element.
+        //Swap it with the current element.
         last = array[m];
         array[m] = array[rand];
         array[rand] = last;
     }
-
+    //returns a shuffeld array
     return array;
 }
 
 
-var cardBack = "images\\Card_back.png";
-var memory_images = [];
+//makes an array of the chosen card images 
+let memory_images = [];
 memory_images[0] = "images\\Moana.png";
 memory_images[1] = "images\\BabyMoana.png";
 memory_images[2] = "images\\Chicken.png";
@@ -28,98 +29,49 @@ memory_images[4] = "images\\Maui.png";
 memory_images[5] = "images\\Grandma.png";
 memory_images = memory_images.concat(memory_images) // duplicates all images in the array
 
+//use the shuffle function on the array, to shuffle cards
 shuffle(memory_images)
-console.log(memory_images)
 
-var cells = document.getElementsByTagName("td")
-var body = document.getElementsByTagName("body")
-for (var i = 0; i in cells; i++) {
-    var card = document.createElement("IMG");
+//makes an array of cells from all "td" tags
+let cells = document.getElementsByTagName("td")
+
+
+//assign a card for each cell and sets its attributes
+for (let i = 0; i in cells; i++) {
+    let card = document.createElement("IMG");
     card.setAttribute("src", memory_images[i]);
     card.setAttribute("height", "210px");
     card.setAttribute("class", "image");
     //    console.log(card);
-    var cell = cells[i];
+    let cell = cells[i];
     //    console.log(cell);
     //    console.log(body);
     cell.appendChild(card);
 }
 
+//changes card's class when clicked to front
 document.querySelectorAll(".back").forEach(item => {
     item.addEventListener('click', event => {
         item.className = "front";
-        var fronts = document.getElementsByClassName("front");
-        console.log(fronts);
+        //makes an array of fronts to check how many cards is faced front.
+        let fronts = document.getElementsByClassName("front");
+        //when the second card reveals, if the src is equal: class changes to hidden. else class returns to back.
         if (fronts.length > 1) {
-            //console.log(fronts[0].innerHTML);
             if (fronts[0].innerHTML === fronts[1].innerHTML) {
                 document.querySelectorAll(".front").forEach(item => {
-                    setTimeout(() => item.className = ".hidden", 750);
+                    setTimeout(() => item.className = "hidden", 750);
                 })
             } else {
                 document.querySelectorAll(".front").forEach(item => {
-                    setTimeout(() => item.className = ".back", 750);
+                    setTimeout(() => item.className = "back", 750);
                 })
             }
-            var backs = document.getElementsByClassName("back");
+            //makes an array of class back: if there's no unrevealed cards: adds a win text instead of table
+            let backs = document.getElementsByClassName("back");
             if (backs.length === 0) {
-                alert("You Won!")
+                document.getElementById("board").innerHTML = "<h1>כל הכבוד!</h1>";
             }
 
         }
     })
 })
-
-
-/*
-for (var i = 0, cell; cell in cells; i++) {
-    document.table.src = memory_images[i]
-        //var x = memory_images[i]
-        //cell.src = '<img name=a src=images\\Card_back.png onclick="flip(this)" height=210px>'
-    console.log(i)
-    var card = document.createElement("IMG");
-    card.setAttribute("src", memory_images[i]);
-    card.setAttribute("height", "210px");
-    document.cell.appendChild(card);
-
-    //cell.innerHTML = '<img src=images\\Grandma.png height=210px>'
-}
-
-/*var table = document.getElementById("board");
-for (var i = 0, cell; cell = table.cells[i]; i++) {
-    console.log(cell[i]);
-    //src = memory_images[i]
-}
-
-
-function flip() {
-
-}
-
-//classes for back, front and hidden
-//document.a.src = cardBack;
-//document.b.src = memory_images[0];
-
-/*class Card {
-    constructor(front) {
-        this.front = front; //words array
-        this.back = "images\\Card_back.png";
-        this.flip = function() {
-            document.c.src = memory_images[2];
-
-            //var len = this.words.length;
-            //for (var i = 0; i < len; i++) {
-            //    this.words[i] += "$";
-        }
-    }
-
-    //console.log("$" + this.words.join(""));
-
-}*/
-
-
-//var x = new Add("this", "is", "awesome");
-//x.print();
-
-//var game = new Card();
-//game.flip();
